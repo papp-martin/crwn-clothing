84d8a4aeaeb9984ac46af9faa073138f65ea0874
Martin Papp <sze0papmar@vavsari.org>
2022-04-28 18:52:25 +0200

    089-működik

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 class SignIn extends React.Component {
     constructor(props) {
@@ -32,7 +33,7 @@ class SignIn extends React.Component {
                 <form onSubmit={this.handleSubmit}>
                     <FormInput name="email" type="email" value={this.state.email} handleChange={this.handleChange} label="Email" required />
                     <FormInput name="password" type="password" value={this.state.password} handleChange={this.handleChange} label="Password" required />
-                    <input type="submit" value="Submit Form" />
+                    <CustomButton type="submit">Sign in</CustomButton>
                 </form>
             </div>
         );
