992acca3135ce4b1ece6a88d003883c76b795261
Martin Papp <sze0papmar@vavsari.org>
2022-05-04 15:24:19 +0200

    147-működik

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,21 +1,10 @@
 import React from 'react';
-import PreviewCollection from '../../components/preview-collection/preview-collection.component';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import { selectCollections } from '../../redux/shop/shop.selectors';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
 const ShopPage = ({ collections }) => (
     <div className='shop-page'>
-        {
-            collections.map(({ id, ...otherCollectionProps }) => (
-                <PreviewCollection key={id} {...otherCollectionProps} />
-            ))
-        }
+        <CollectionsOverview />
     </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
