66893ce89ed3d5974ab7705fee5dc3b1a0952d51
Martin Papp <sze0papmar@vavsari.org>
2022-05-04 15:58:33 +0200

    150-működik

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import './collection.styles.scss';
+import { connect } from "react-redux";
 import CollectionItem from "../../components/collection-item/collection-item.component";
+import { selectCollection } from "../../redux/shop/shop.selectors";
 
-
-const CollectionPage = ({ match }) => {
-    console.log(match.params.collectionId);
+const CollectionPage = ({ collection }) => {
+    console.log(collection);
     return(
     <div className="collection-page">
         <h2>collection PAGE</h2>
@@ -12,4 +13,8 @@ const CollectionPage = ({ match }) => {
     );
 };
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CollectionPage);
