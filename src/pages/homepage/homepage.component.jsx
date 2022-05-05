31591d09a5728edda073851120827c3399f70ff6
Martin Papp <sze0papmar@vavsari.org>
2022-05-05 18:45:20 +0200

    169-működik

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 import Directory from '../../components/directory/directory.component';
 
-import './homepage.styles.scss';
+import { HomePageConatiner } from './homepage.styles';
+
 
 const HomePage = () => (
-  <div className='homepage'>
+  <HomePageConatiner>
     <Directory />
-  </div>
+  </HomePageConatiner>
 );
 
 export default HomePage;
